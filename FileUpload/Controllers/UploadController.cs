5ef8b820b47a9fce5d03047e3f18be8aad186b6a
anthonychui <anthonychui@microsoft.com>
2021-01-18 10:44:21 -0800

    Add initial code for file upload

---
+++ FileUpload/Controllers/UploadController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting.Internal;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileUpload.Controllers
+{
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpGet]
+        [Route("test")]
+        public async Task<string> Test([FromForm] IFormFile file)
+        {
+            return "hello";
+        }
+
+        [HttpPost]
+        [Route("api/upload/file")]
+        public async Task<string> UploadFile([FromForm] IFormFile file)
+        {
+            string fName = file.FileName;
+            string path = Path.Combine($"C:/Users/anthonychui/Downloads/{Guid.NewGuid()} {file.FileName}");
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            return file.FileName;
+        }
+
+    }
+}
